9c9685f964bd5cb3680bcca07465aca2e8fcaf42
gitSuyog <codesuyog0@gmail.com>
2024-07-11 23:11:27 +0545

    add tasks on progress

--- admin-fe/components/ColumnContainer.tsx
+++ admin-fe/components/ColumnContainer.tsx
@@ -1,22 +1,89 @@
+"use client";
+
 import { Column, Id } from "@/types";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { useState } from "react";
 
 interface Props {
   column: Column;
   deleteColumn: (id: Id) => void;
+  updateColumn: (id: Id, title: string) => void;
 }
 
 const ColumnContainer = (props: Props) => {
-  const { column, deleteColumn } = props;
+  const { column, deleteColumn, updateColumn } = props;
+
+  const [editMode, setEditMode] = useState(false);
+
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: column.id,
+    data: {
+      type: "Column",
+      column,
+    },
+    disabled: editMode,
+  });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="mt-[10rem] bg-white shadow-lg w-[21.9rem] h-[31.29rem] max-h[31.29rem] rounded-lg flex flex-col opacity-55 border border-black border-dashed"
+      ></div>
+    );
+  }
   return (
-    <div className=" mt-[10rem] bg-white shadow-lg w-[21.9rem] h-[31.29rem] max-h[31.29rem] rounded-lg flex flex-col ">
+    <div
+      ref={setNodeRef}
+      style={style}
+      className=" mt-[10rem] bg-white shadow-lg w-[21.9rem] h-[31.29rem] max-h[31.29rem] rounded-lg flex flex-col mb-[5rem] "
+    >
       {/* Column Title */}
 
-      <div className=" text-md h-[3.8rem] cursor-grab rounded-md rounded-b-none p-3 font-bold flex items-center justify-between">
+      <div
+        {...attributes}
+        {...listeners}
+        onClick={() => {
+          setEditMode(true);
+        }}
+        className=" text-md h-[3.8rem] cursor-grab rounded-md rounded-b-none p-3 font-bold flex items-center justify-between"
+      >
         <div className="flex gap-2 ">
           <div className="flex justify-center items-center bg-white px-2 py-1 text-sm rounded-full">
             0
           </div>
-          {column.title}
+
+          {!editMode && column.title}
+          {editMode && (
+            <input
+              value={column.title}
+              onChange={(e) => updateColumn(column.id, e.target.value)}
+              autoFocus
+              onBlur={() => {
+                setEditMode(false);
+              }}
+              onKeyDown={(e) => {
+                if (e.key !== "Enter") {
+                  return;
+                }
+                setEditMode(false);
+              }}
+            />
+          )}
         </div>
         <button
           onClick={() => {
@@ -29,7 +96,7 @@ const ColumnContainer = (props: Props) => {
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={1.5}
-            className="w-6 h-6   "
+            className="w-6 h-6"
           >
             <path
               strokeLinecap="round"
@@ -42,7 +109,23 @@ const ColumnContainer = (props: Props) => {
       {/* Column task container */}
       <div className="flex flex-grow">Content</div>
       {/* Column footer */}
-      <div>footer</div>
+      <button className="flex items-center gap-2 justify-center p-2 bg-slate-200   transition duration-200 ease-out hover:bg-black hover:text-white rounded-lg rounded-t-none ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+        Add
+      </button>
     </div>
   );
 };
--- admin-fe/components/KanbanBoard.tsx
+++ admin-fe/components/KanbanBoard.tsx
@@ -1,12 +1,38 @@
 "use client";
 
-import { Column, Id } from "@/types";
-import React from "react";
+import { Column, Id, Task } from "@/types";
+import React, { useMemo } from "react";
 import { useState } from "react";
 import ColumnContainer from "./ColumnContainer";
+
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { arrayMove, SortableContext } from "@dnd-kit/sortable";
+import { createPortal } from "react-dom";
 const KanbanBoard = () => {
   const [columns, setColumns] = useState<Column[]>([]);
+
+  const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
   console.log(columns);
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const [activeColumn, setActiveColumn] = useState<Column | null>(null);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 3,
+      },
+    })
+  );
 
   const createNewColumn = () => {
     const columnToAdd: Column = {
@@ -20,39 +46,106 @@ const KanbanBoard = () => {
     const filteredColumns = columns.filter((col) => col.id !== id);
     setColumns(filteredColumns);
   };
+
+  const updateColumn = (id: Id, title: string) => {
+    const newColumns = columns.map((col) => {
+      if (col.id !== id) return col;
+      return { ...col, title };
+    });
+
+    setColumns(newColumns);
+  };
+
+  const onDragStart = (event: DragStartEvent) => {
+    console.log("DRAG START", event);
+
+    if (event.active.data.current?.type === "Column") {
+      setActiveColumn(event.active.data.current.column);
+      return;
+    }
+  };
+
+  const onDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+    if (!over) return;
+
+    const activeColumnId = active.id;
+    const overColumnId = over.id;
+
+    if (activeColumnId === overColumnId) {
+      return;
+    }
+
+    setColumns((columns) => {
+      const activeColumnIndex = columns.findIndex(
+        (col) => col.id === activeColumnId
+      );
+
+      const overColumnIndex = columns.findIndex(
+        (col) => col.id === overColumnId
+      );
+
+      return arrayMove(columns, activeColumnIndex, overColumnIndex);
+    });
+  };
   return (
-    <div className="px-6 m-auto flex min-h-screen w-full items-start overflow-x-auto overflow-y-hidden">
-      <div className="flex gap-4">
+    <div className="  px-6 m-auto flex min-h-screen w-full items-start overflow-x-auto overflow-y-hidden">
+      <DndContext
+        sensors={sensors}
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
+      >
         <div className="flex gap-4">
-          {columns.map((col) => (
-            <div>
-              <ColumnContainer column={col} deleteColumn={deleteColumn} />
-            </div>
-          ))}
-        </div>
-        <button
-          onClick={() => {
-            createNewColumn();
-          }}
-          className=" mt-[10rem] flex gap-2 items-center justify-center h-[3rem] w-[18rem] min-w-[18rem] cursor-pointer rounded-lg bg-white shadow-lg    transition duration-200 ease-in-out transform hover:scale-105    "
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="h-6 w-6"
+          <div className="flex gap-4">
+            <SortableContext items={columnsId}>
+              {columns.map((col) => (
+                <div>
+                  <ColumnContainer
+                    key={col.id}
+                    column={col}
+                    deleteColumn={deleteColumn}
+                    updateColumn={updateColumn}
+                  />
+                </div>
+              ))}
+            </SortableContext>
+          </div>
+          <button
+            onClick={() => {
+              createNewColumn();
+            }}
+            className=" mt-[10rem] flex gap-2 items-center justify-center h-[3rem] w-[18rem] min-w-[18rem] cursor-pointer rounded-lg bg-white shadow-lg    transition duration-200 ease-in-out transform hover:scale-105    "
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
-          Add Column
-        </button>
-      </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            Add Column
+          </button>
+        </div>
+        {createPortal(
+          <DragOverlay>
+            {activeColumn && (
+              <ColumnContainer
+                column={activeColumn}
+                deleteColumn={deleteColumn}
+                updateColumn={updateColumn}
+              />
+            )}
+          </DragOverlay>,
+          document.body
+        )}
+      </DndContext>
     </div>
   );
 };
